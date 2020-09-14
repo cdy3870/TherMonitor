398f1eca55c7398718bd5ba3e05da0da28b47ad0
Calvin <cdy3870@gmail.com>
2020-09-14 14:53:06 -0400

    added sketch file

---
+++ Hardware Code/detect/detect.ino
@@ -0,0 +1,16 @@
+#include <ESP8266WiFi.h">
+#include <<span class="TextRun Highlight BCX0 SCXW174472232" lang="EN-US" xml:lang="EN-US" data-contrast="auto"><span class="SpellingError BCX0 SCXW174472232">FirebaseArduino.h></span></span>
+// Set these to run example. 
+/*#define FIREBASE_HOST "example.firebaseio.com" 
+#define FIREBASE_AUTH "token_or_secret" 
+#define WIFI_SSID "SSID" 
+#define WIFI_PASSWORD "PASSWORD" */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
