5edfca89f8145fda0bc4eb46537ea776490a5b5d
Daniel Mingey <you@example.com>
2020-09-27 16:22:36 -0400

    added database subscription for newly created Events

--- TherMonitor/TherMonitor/FirstViewController.swift
+++ TherMonitor/TherMonitor/FirstViewController.swift
@@ -7,15 +7,47 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class FirstViewController: UIViewController {
 
     @IBOutlet weak var currentRoomCap: UILabel!
+
+//    var ref:FIRDatabaseReference?
     
+//    var postData = [String]()
+
+//    var databaseHandle:FIRDatabaseHandle?
+
+//    var total_room_ocupancy = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+   
+//	ref = FIRDatabase.database().reference()
+
+//	databaseHandle = ref?.child("Events").observe(.childAdded, with: { (snapshot) in 
+
+		// code to execute when a child is added under "Events"
+
+		// take the value from the snapshot and add it to an array
+		
+		// so here snapshot will contain the information from the database under
+		// Events
+
+		// i think what we want to do is something like
+		// if snapshot.child direction.value = "entry" => self.total_room_occupancy++
+		// else if snapshot.child direction.value = "exit" => self.total_room_occupancy--
+
+		
+		// here we use our new room occupancy data as the variable for the View
+		// we are going to need to refresh
+		
+		
+
+//	})
+     }
 
 
     
