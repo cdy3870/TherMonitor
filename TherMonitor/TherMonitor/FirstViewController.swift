b1b3c4d9ee421887171431d8a8515a34b188ace2
Alyssa Jordan <alj81@pitt.edu>
2020-09-27 19:17:57 -0400

    database app hooked up

--- TherMonitor/TherMonitor/FirstViewController.swift
+++ TherMonitor/TherMonitor/FirstViewController.swift
@@ -13,43 +13,51 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var currentRoomCap: UILabel!
 
-//    var ref:FIRDatabaseReference?
+    var ref:DatabaseReference?
     
-//    var postData = [String]()
+    var postData = [String]()
 
-//    var databaseHandle:FIRDatabaseHandle?
+    var databaseHandle:DatabaseHandle?
 
-//    var total_room_ocupancy = 0
+    var total_room_occupancy = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
    
-//	ref = FIRDatabase.database().reference()
+        ref = Database.database().reference()
 
-//	databaseHandle = ref?.child("Events").observe(.childAdded, with: { (snapshot) in 
+        databaseHandle = ref?.child("Test").observe(.childAdded, with: { (snapshot) in
+            
+            
+            // code to execute when a child is added under "Events"
 
-		// code to execute when a child is added under "Events"
+            // take the value from the snapshot and add it to an array
+            
+            // so here snapshot will contain the information from the database under
+            // Events
 
-		// take the value from the snapshot and add it to an array
-		
-		// so here snapshot will contain the information from the database under
-		// Events
+            let post = snapshot.value as? String
+            // i think what we want to do is something like
+            if (post == "entry") {
+                self.total_room_occupancy+=1
+            }
+            else if (post == "exit"){
+                self.total_room_occupancy-=1
+            }
+            // else if snapshot.child direction.value = "exit" => self.total_room_occupancy--
 
-		// i think what we want to do is something like
-		// if snapshot.child direction.value = "entry" => self.total_room_occupancy++
-		// else if snapshot.child direction.value = "exit" => self.total_room_occupancy--
+            
+            // here we use our new room occupancy data as the variable for the View
+            // we are going to need to refresh
+            
+            self.currentRoomCap.text = String(self.total_room_occupancy)
 
-		
-		// here we use our new room occupancy data as the variable for the View
-		// we are going to need to refresh
-		
-		
-
-//	})
+        })
+        
+        //currentRoomCap.text = String(total_room_occupancy)
      }
 
 
-    
 }
 
