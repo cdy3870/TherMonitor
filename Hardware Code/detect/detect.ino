5c00c91610d2d67ff39f598d0203c35ebedca6b3
Calvin <cdy3870@gmail.com>
2020-10-08 00:17:32 -0400

    Added second sensor code

--- Hardware Code/detect/detect.ino
+++ Hardware Code/detect/detect.ino
@@ -1,5 +1,3 @@
-
-
 #include <Firebase_Arduino_WiFiNINA.h>
 #include <Firebase_Arduino_WiFiNINA_HTTPClient.h>
 #include <WiFiNINA.h>
@@ -23,9 +21,17 @@
 
 FirebaseData firebaseData;
 
-int analogPin = A1; 
+/* Analog Pins */
+int analogPin1 = A1; 
+//int analogPin2 = A2;
 
-int val = 0; 
+/* Digital Pins */
+//int interruptPin = 2;
+//int digitalPin = 3;
+
+/* Times */
+int time1 = 0;
+int time2 = 0;
 
 int count = 1;
 
@@ -33,18 +39,11 @@ void setup() {
   /* Serial Connection */
   Serial.begin(9600);
 
-  /* Sensor Setup */
-//   Wire.begin(0x48);
-//  if(!infra_sensor.begin())
-//  {
-//    Serial.println("Cannot connect to TMP102.");
-//    Serial.println("Is the board connected? Is the device ID correct?");
-//    while(1);
-//  }
+  /* Interrupt setup */
+//  pinMode(interruptPin, INPUT_PULLUP);
+//  pinMode(digitalPin, OUTPUT);
+//  attachInterrupt(digitalPinToInterrupt(interruptPin, uploadToFirebase, CHANGE);
   
-//  infra_sensor.setHighTempF(82.0);  //set T_HIGH, the upper limit to trigger the alert on
-//  infra_sensor.setLowTempF(81.0);  //set T_LOW, the lower limit to shut turn off the alert
-
   /* Wi-Fi Setup */ 
   Serial.println(WiFi.begin(WIFI_SSID, WIFI_PASSWORD));
   while (WiFi.status() != 3) { 
@@ -64,38 +63,53 @@ void setup() {
 }
 
 void loop() {
- 
-  /* Reading from sensor */
-//  infra_sensor.wakeup(); // Turn sensor on to start temperature measurement. Current consumtion typically ~10uA.
-//   temperature = infra_sensor.readTempF(); // read temperature data
-//  Serial.print(" Temperature reading: "); // print to serial
-//  Serial.println(temperature);
-  val = analogRead(analogPin);
-  Serial.println(val);
-  if(val > 210){
-    Serial.print(val);
+  /* Analog Readings */
+  int sensorVal1 = 0;
+//  int sensorVal2 = 0;
+
+  /* Reading from sensors */  
+  sensorVal1 = analogRead(analogPin1);
+  Serial.println(sensorVal1);
+//  sensorVal2 = analogRead(analogPin2);
+//  Serial.println(sensorVal1);
+
+  checkSensor(sensorVal1, analogPin1, 1);
+//  checkSensor(sensorVal2, analogPin2, 2);
+  
+
+}
+
+int checkSensor(int sensorVal, int analogPin, int id){  
+  /* Detection "Algorithm" */
+  if(sensorVal > 210){
+    Serial.print(sensorVal);
     Serial.println(" Entry");
+
+//    digitalWrite(digitalPin, HIGH);
+    if(id = 1) time1 = millis();
+    else time2 = millis();
+    if(time1 > time2)uploadToFirebase("Entry");
+    else uploadToFirebase("Exit");
     
-    /* Interfacing with Firebase */
-    String path = "/Data";
-    String count2 = String(count);
-    count += 1;
-    String jsonData = "{\"Timestamp\":\"231321546\", \"Direction\":\"Entry\", \"Count\":\"" + count2 + "\"}";
-    if(Firebase.setJSON(firebaseData, path, jsonData)){ // set value in Firebase 
-      //Serial.print("data successfully pushed to Firebase");
-    }  
-    else{
-      //Serial.println("Error reason: " + firebaseData.errorReason());
-    }
-    while(val > 210){
-      val = analogRead(analogPin);
+    while(sensorVal > 210){
+      sensorVal = analogRead(analogPin);
     }
     delay(500);
   }
-    /* Detection "Algorithm" */
-    //if(temperature) > 70 
-
-      
- // infra_sensor.sleep(); // Place sensor in sleep mode to save power. Current consumtion typically <0.5uA.
-//  delay(1000);  // Wait 1000ms
 }
+
+void uploadToFirebase(String dir){
+  /* Interfacing with Firebase */
+  String path = "/Data";
+  String count2 = String(count);
+  count += 1;
+  String jsonData = "{\"Timestamp\":\"231321546\", \"Direction\":\"Entry\", \"Count\":\"" + count2 + "\"}";
+  if(Firebase.setJSON(firebaseData, path, jsonData)){ // set sensorVal1ue in Firebase 
+    Serial.println("data successfully pushed to Firebase");
+  }  
+  else{
+    Serial.println("Error reason: " + firebaseData.errorReason());
+  }
+}
+
+
