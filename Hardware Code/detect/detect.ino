b8620420499c85baab0233c4774711533f8f461b
Calvin <cdy3870@gmail.com>
2020-10-14 00:07:40 -0400

    restructuring again

--- Hardware Code/detect/detect.ino
+++ Hardware Code/detect/detect.ino
@@ -1,4 +1,3 @@
-
 #include <Firebase_Arduino_WiFiNINA.h>
 #include <Firebase_Arduino_WiFiNINA_HTTPClient.h>
 #include <WiFiNINA.h>
@@ -6,40 +5,41 @@
 #include <SparkFunTMP102.h>
 
 /* Board
- * Install megaAVR boards and select Arduino UNO Rev2  
- */
- 
-/* Libs 
- *  WiFiNINA
- *  SparkFunTMP102
- *  FirebaseArduino (that supports WiFiNINA)
- */
- 
-#define WIFI_SSID "372 South Bouquet C" 
+   Install megaAVR boards and select Arduino UNO Rev2
+*/
+
+/* Libs
+    WiFiNINA
+    SparkFunTMP102
+    FirebaseArduino (that supports WiFiNINA)
+*/
+
+#define WIFI_SSID "372 South Bouquet C"
 #define WIFI_PASSWORD "Janvar1226"
-#define FIREBASE_HOST "thermonitor-f2d55.firebaseio.com" 
-#define FIREBASE_AUTH "4ipmlNTVDKQW2rWdVPDPe1NS9aCPuq8x1GpVau7E" 
+#define FIREBASE_HOST "thermonitor-f2d55.firebaseio.com"
+#define FIREBASE_AUTH "4ipmlNTVDKQW2rWdVPDPe1NS9aCPuq8x1GpVau7E"
 
 FirebaseData firebaseData;
 
 /* Analog Pins */
-int analogPin1 = A1; 
+int analogPin1 = A2;
 int analogPin2 = A3;
 
-/* Digital Pins */
-//int interruptPin = 2;
-//int digitalPin = 3;
+/* Analog readings */
+int sensorVal1 = 0;
+int sensorVal2 = 0;
+
+/* Distances */
+float distance1 = 0;
+float distance2 = 0;
 
 /* Times */
 unsigned long time1 = 0;
 unsigned long time2 = 0;
-<<<<<<< HEAD
 
 /* Other constants */
 float voltage = 5.0;
 int max_analog_value = 1023;
-=======
->>>>>>> 4b1f33f31cf09c39e0165cf17c3fb1786ef224eb
 
 int count = 1;
 
@@ -47,88 +47,52 @@ void setup() {
   /* Serial Connection */
   Serial.begin(9600);
 
-  /* Interrupt setup */
-//  pinMode(interruptPin, INPUT_PULLUP);
-//  pinMode(digitalPin, OUTPUT);
-//  attachInterrupt(digitalPinToInterrupt(interruptPin, uploadToFirebase, CHANGE);
-  
-  /* Wi-Fi Setup */ 
+  /* Wi-Fi Setup */
   Serial.println(WiFi.begin(WIFI_SSID, WIFI_PASSWORD));
-  while (WiFi.status() != 3) { 
-    Serial.print("Not connected to Wi-Fi"); 
+  while (WiFi.status() != 3) {
+    Serial.print("Not connected to Wi-Fi");
     Serial.print(WiFi.status());
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    delay(1000); 
-    
-  } 
-  Serial.println(); 
-  Serial.print("Connected: "); 
-  Serial.println(WiFi.localIP()); 
-  
+    delay(1000);
+
+  }
+  Serial.println();
+  Serial.print("Connected: ");
+  Serial.println(WiFi.localIP());
+
   /* Firebase Setup */
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH, WIFI_SSID, WIFI_PASSWORD);
   Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  /* Analog Readings */
-  int sensorVal1 = 0;
-  int sensorVal2 = 0;
-
-  /* Reading from sensors */  
+  /* Reading from sensors */
   sensorVal1 = analogRead(analogPin1);
   Serial.println(sensorVal1);
-//  sensorVal2 = analogRead(analogPin2);
-//  Serial.println(sensorVal1);
+  sensorVal2 = analogRead(analogPin2);
+  Serial.println(sensorVal1);
 
-//  getTriggerTimes(1);
-//  getTriggerTimes(2);
-//  checkSensor(sensorVal1, analogPin1, 1);
-//  checkSensor(sensorVal2, analogPin2, 2);
-  float volts = sensorVal1*(voltage/max_analog_value);
+  /* Distance calculation */
+  distance1 = getDistance(sensorVal1);
+  distance2 = getDistance(sensorVal2);
+
+  /* Setting trigger times */
+  setTriggerTime(1, distance1);
+  setTriggerTime(2, distance2);
+  checkSensor();
+}
+
+int getDistance(int sensorVal){
+  float volts = sensorVal1 * (voltage / max_analog_value);
   // From Sharp.h library
-  float distance = 29.988*pow(volts, -1.173);   
-  Serial.print(distance);     
+  float distance = 29.988 * pow(volts, -1.173);
+  Serial.print(distance);
   Serial.println(" cm");
-  delay(100);    
-
+  return distance;  
 }
 
-//int getTriggerTimes(int id){
-//  if(sensorVal > 240){  
-//    if(id = 1){
-//      time1 = millis();
-//      Serial.print("Time 1: ");
-//      Serial.println(time1);
-//      Serial.print("Time 2: ");
-//      Serial.println(time2);
-//    }
-//    else{
-//      time2 = millis();
-//      Serial.print("Time 1: ");
-//      Serial.println(time1);
-//      Serial.print("Time 2: ");
-//      Serial.println(time2);
-//    }
-//  }
-//}
-
-int checkSensor(int analogPin){
-    if(time1 > time2)uploadToFirebase("Entry");
-    else uploadToFirebase("Exit");
-    
-//    while(sensorVal > 210){
-//      sensorVal = analogRead(analogPin);
-//    } 
-}
-
-int checkSensor(int sensorVal, int analogPin, int id){  
-  /* Detection "Algorithm" */
-  if(sensorVal > 240){
-    Serial.print(id);
-    Serial.println(" Entry/Exit");
-
-//    digitalWrite(digitalPin, HIGH);
+int setTriggerTime(int id, float distance){
+  if(distance < 70){
     if(id = 1){
       time1 = millis();
       Serial.print("Time 1: ");
@@ -139,35 +103,32 @@ int checkSensor(int sensorVal, int analogPin, int id){
     else{
       time2 = millis();
       Serial.print("Time 1: ");
-<<<<<<< HEAD
       Serial.println(time1);
-=======
-       Serial.println(time1);
->>>>>>> 4b1f33f31cf09c39e0165cf17c3fb1786ef224eb
       Serial.print("Time 2: ");
       Serial.println(time2);
     }
-    if(time1 > time2)uploadToFirebase("Entry");
-    else uploadToFirebase("Exit");
-    
-    //while(sensorVal > 210){
-    //  sensorVal = analogRead(analogPin);
-    //}
-    delay(500);
   }
 }
 
-void uploadToFirebase(String dir){
-  /* Interfacing with Firebase */
+int checkSensor() {
+  if (time1 > time2) uploadToFirebase("Entry");
+  else uploadToFirebase("Exit");
 
+  //    while(sensorVal > 210){
+  //      sensorVal = analogRead(analogPin);
+  //    }
+}
+
+void uploadToFirebase(String dir) {
+  /* Interfacing with Firebase */
   String path = "/Data";
   String count2 = String(count);
   count += 1;
   String jsonData = "{\"Timestamp\":\"231321546\", \"Direction\":\"" + dir + "\", \"Count\":\"" + count2 + "\"}";
-  if(Firebase.setJSON(firebaseData, path, jsonData)){ // set sensorVal1ue in Firebase 
+  if (Firebase.setJSON(firebaseData, path, jsonData)) { // set sensorVal1ue in Firebase
     Serial.println("data successfully pushed to Firebase");
-  }  
-  else{
+  }
+  else {
     Serial.println("Error reason: " + firebaseData.errorReason());
   }
 }
