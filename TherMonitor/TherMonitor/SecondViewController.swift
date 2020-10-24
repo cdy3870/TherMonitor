a9c331db1533ae15876f7a817c8ade0b2c384ae3
Alyssa Jordan <alj81@pitt.edu>
2020-10-24 19:45:36 -0400

    second page

--- TherMonitor/TherMonitor/SecondViewController.swift
+++ TherMonitor/TherMonitor/SecondViewController.swift
@@ -24,7 +24,9 @@ class SecondViewController: UIViewController {
 
     @IBOutlet weak var barChart: BarChartView!
     @IBOutlet weak var curDay: UILabel!
-
+    
+    @IBOutlet weak var timingSelection: UISegmentedControl!
+    
     var databaseHandle: DatabaseHandle?
     var ref: DatabaseReference?
 
@@ -103,7 +105,9 @@ class SecondViewController: UIViewController {
             curHour = hour + 1
         }
         
-        let allHours = ["1am","2am","3am","4am","5am","6am","7am","8am","9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm","12am"]
+        //let allHours = ["1am","2am","3am","4am","5am","6am","7am","8am","9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm","12am"]
+        let hours = ["","10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+
         //let intHours = [curHour-6...curHour+4]
         //let hours = Array(allHours[curHour-6...curHour+4])
     
@@ -113,28 +117,22 @@ class SecondViewController: UIViewController {
         databaseHandle = ref?.child("WeeklyHourlyAverages").child(cDay).observe(.value, with: {(snapshot) in
             var i = 0
             for cDay in snapshot.children.allObjects as![DataSnapshot]{
-                //print("i: \(i)")
                 let hourObj = cDay.value as? [String: Any]
                 let hourAvg = hourObj?["average"]
                 self.dbHours[i] = hourAvg as! Double
-                //print("db hour \(self.dbHours[i])")
                 i+=1
-                
-                //print("Hour Object: \(hourObj)")
                 
             }
             
             self.set = BarChartDataSet(entries: [
-                    BarChartDataEntry(x: Double(1),y: self.dbHours[0]),
-                    BarChartDataEntry(x: Double(2),y: self.dbHours[1]),
-                    BarChartDataEntry(x: Double(3),y: self.dbHours[2]),
-                    BarChartDataEntry(x: Double(4),y: self.dbHours[3]),
-                    BarChartDataEntry(x: Double(5),y: self.dbHours[4]),
-                    BarChartDataEntry(x: Double(6),y: self.dbHours[5]),
-                    BarChartDataEntry(x: Double(7),y: self.dbHours[6]),
-                    BarChartDataEntry(x: Double(8),y: self.dbHours[7]),
-                    BarChartDataEntry(x: Double(9),y: self.dbHours[8]),
-                    BarChartDataEntry(x: Double(10),y: self.dbHours[9])
+                    BarChartDataEntry(x: Double(1),y: self.dbHours[10]),
+                    BarChartDataEntry(x: Double(2),y: self.dbHours[11]),
+                    BarChartDataEntry(x: Double(3),y: self.dbHours[12]),
+                    BarChartDataEntry(x: Double(4),y: self.dbHours[13]),
+                    BarChartDataEntry(x: Double(5),y: self.dbHours[14]),
+                    BarChartDataEntry(x: Double(6),y: self.dbHours[15]),
+                    BarChartDataEntry(x: Double(7),y: self.dbHours[16]),
+                    BarChartDataEntry(x: Double(8),y: self.dbHours[17])
               ], label: "Hour")
             
             let data = BarChartData(dataSet: self.set)
@@ -149,11 +147,119 @@ class SecondViewController: UIViewController {
         barChart.legend.enabled = false
         barChart.xAxis.labelPosition = .bottom
         barChart.xAxis.drawGridLinesEnabled = false
-        barChart.xAxis.setLabelCount(9, force: false)
-        //barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values:hours)
+        barChart.xAxis.setLabelCount(7, force: false)
+        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values:hours)
 
 
     }
+    
+    @IBAction func switchSel(_ sender: Any) {
+        
+        let currentDate = Date()
+        let myCalendar = Calendar(identifier: .gregorian)
+        let weekDay = myCalendar.component(.weekday, from: currentDate)
+        let cDay = String(weekDay-1)
+        
+        var hours = ["","10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+        
+        switch timingSelection.selectedSegmentIndex{
+            case 0:
+                hours = ["","12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am"]
+                break
+            case 1:
+                hours = ["","8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm"]
+                break
+            case 2:
+                hours = ["", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"]
+                break
+            default:
+                break
+        
+        }
+        
+        ref = Database.database().reference()
+        databaseHandle = ref?.child("WeeklyHourlyAverages").child(cDay).observe(.value, with: {(snapshot) in
+            var i = 0
+            for cDay in snapshot.children.allObjects as![DataSnapshot]{
+                let hourObj = cDay.value as? [String: Any]
+                let hourAvg = hourObj?["average"]
+                self.dbHours[i] = hourAvg as! Double
+                i+=1
+                
+            }
+            switch self.timingSelection.selectedSegmentIndex{
+            case 0:
+                //print("0")
+                self.set = BarChartDataSet(entries: [
+                        BarChartDataEntry(x: Double(1),y: self.dbHours[0]),
+                        BarChartDataEntry(x: Double(2),y: self.dbHours[1]),
+                        BarChartDataEntry(x: Double(3),y: self.dbHours[2]),
+                        BarChartDataEntry(x: Double(4),y: self.dbHours[3]),
+                        BarChartDataEntry(x: Double(5),y: self.dbHours[4]),
+                        BarChartDataEntry(x: Double(6),y: self.dbHours[5]),
+                        BarChartDataEntry(x: Double(7),y: self.dbHours[6]),
+                        BarChartDataEntry(x: Double(8),y: self.dbHours[7])
+                  ], label: "Hour")
+                
+                let data = BarChartData(dataSet: self.set)
+                
+
+                self.barChart.data = data
+                break
+            case 1:
+                //print("1")
+                self.set = BarChartDataSet(entries: [
+                        BarChartDataEntry(x: Double(1),y: self.dbHours[8]),
+                        BarChartDataEntry(x: Double(2),y: self.dbHours[9]),
+                        BarChartDataEntry(x: Double(3),y: self.dbHours[10]),
+                        BarChartDataEntry(x: Double(4),y: self.dbHours[11]),
+                        BarChartDataEntry(x: Double(5),y: self.dbHours[12]),
+                        BarChartDataEntry(x: Double(6),y: self.dbHours[13]),
+                        BarChartDataEntry(x: Double(7),y: self.dbHours[14]),
+                        BarChartDataEntry(x: Double(8),y: self.dbHours[15])
+                  ], label: "Hour")
+                
+                let data = BarChartData(dataSet: self.set)
+                
+
+                self.barChart.data = data
+                break
+            case 2:
+                //print("2")
+                self.set = BarChartDataSet(entries: [
+                        BarChartDataEntry(x: Double(1),y: self.dbHours[16]),
+                        BarChartDataEntry(x: Double(2),y: self.dbHours[17]),
+                        BarChartDataEntry(x: Double(3),y: self.dbHours[18]),
+                        BarChartDataEntry(x: Double(4),y: self.dbHours[19]),
+                        BarChartDataEntry(x: Double(5),y: self.dbHours[20]),
+                        BarChartDataEntry(x: Double(6),y: self.dbHours[21]),
+                        BarChartDataEntry(x: Double(7),y: self.dbHours[22]),
+                        BarChartDataEntry(x: Double(8),y: self.dbHours[23])
+                  ], label: "Hour")
+                
+                let data = BarChartData(dataSet: self.set)
+                
+                self.barChart.data = data
+                break
+            default:
+                print("d")
+                break
+            }
+
+            
+        })
+        
+        barChart.rightAxis.enabled = false
+        barChart.leftAxis.enabled = false
+        barChart.legend.enabled = false
+        barChart.xAxis.labelPosition = .bottom
+        barChart.xAxis.drawGridLinesEnabled = false
+        barChart.xAxis.setLabelCount(7, force: false)
+        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values:hours)
+
+        
+    }
+    
 
 
 }
