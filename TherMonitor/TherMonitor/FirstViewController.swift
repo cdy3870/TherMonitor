b92ecfbcd23d47ac08c2b72be35bf98166410f21
Alyssa Jordan <alj81@pitt.edu>
2020-11-02 15:20:25 -0500

    max cap

--- TherMonitor/TherMonitor/FirstViewController.swift
+++ TherMonitor/TherMonitor/FirstViewController.swift
@@ -12,7 +12,8 @@ import FirebaseDatabase
 class FirstViewController: UIViewController {
 
     @IBOutlet weak var currentRoomCap: UILabel!
-
+    @IBOutlet weak var maxRoomCap: UILabel!
+    
     var ref:DatabaseReference?
 
     var postData = [String]()
@@ -27,46 +28,8 @@ class FirstViewController: UIViewController {
 
         ref = Database.database().reference()
         
-        databaseHandle = ref?.child("Rooms").observe(.value, with: {(snapshot) in
-            var roomArr: Array<Double> = Array(repeating: 0, count: 10)
-            var i = 0
-            for r in snapshot.children.allObjects as![DataSnapshot]{
-                let roomObj = r.value as? [String: Any]
-                let roomOcc = roomObj?["currentOccupancy"]
-                roomArr[i] = roomOcc as! Double
-                i+=1
-            }
-            self.currentRoomCap.text = String(format: "%g", roomArr[0])
-            
-        })
         
-        /*databaseHandle = ref?.child("Rooms").observe(.value, with: { (snapshot) in
-            
-            for r in snapshot.children.allObjects as![DataSnapshot]{
-                let rD = r.value as? [String: Any]
-                let val = rD?["currentOccupancy"]
-                self.currentRoomCap.text = val as! String
-            }
-            
-            
-            //let r = snapshot.value as![DataSnapshot]
-            //let rD = snapshot.value as? [String: Any]
-            
-            //let values = rD?["currentOccupancy"]
-            
-            
-            
-            
-            
-            
-            
-            /*for r in snapshot.children. as![DataSnapshot]{
-                let rNum = r.value as? [String
-                self.currentRoomCap.text = rNum
-            }*/
-            
-        })*/
-        /*databaseHandle = ref?.child("Events").observe(.childAdded, with: { (snapshot) in
+        databaseHandle = ref?.child("Events").observe(.childAdded, with: { (snapshot) in
 
             let values = snapshot.value as? NSDictionary
             // i think what we want to do is something like
@@ -80,9 +43,22 @@ class FirstViewController: UIViewController {
             // we are going to need to refresh
 
             self.currentRoomCap.text = String(self.total_room_occupancy)
-            //self.currentRoomCap.text = values
 
-        })*/
+
+        })
+        
+        databaseHandle = ref?.child("MaxCapacity").observe(.value, with: {(snapshot) in
+            var roomArr: Array<Double> = Array(repeating: 0, count: 10)
+            var i = 0
+            for r in snapshot.children.allObjects as![DataSnapshot]{
+                let roomObj = r.value as? [String: Any]
+                let roomOcc = roomObj?["max"]
+                roomArr[i] = roomOcc as! Double
+                i+=1
+            }
+            self.maxRoomCap.text = String(format: "%g", roomArr[0])
+            
+        })
 
 
      }
