4ebe4fcc7ad7b02d76605fe0bf08d26ce4072acb
Daniel Mingey <you@example.com>
2020-11-02 21:49:44 -0500

    implemented entry state flow machine

--- Hardware Code/detect/detect.ino
+++ Hardware Code/detect/detect.ino
@@ -19,6 +19,14 @@
 #define FIREBASE_HOST "thermonitor-f2d55.firebaseio.com"
 #define FIREBASE_AUTH "4ipmlNTVDKQW2rWdVPDPe1NS9aCPuq8x1GpVau7E"
 
+#define STATE_INIT 0
+#define STATE_EXIT_1 1
+#define STATE_EXIT_2 2
+#define STATE_EXIT_3 3
+#define STATE_ENTRY_1 4
+#define STATE_ENTRY_2 5
+#define STATE_ENTRY_3 6
+
 FirebaseData firebaseData;
 
 /* Analog Pins */
@@ -43,7 +51,7 @@ int max_analog_value = 1023;
 
 bool if_triggered_1 = false;
 bool if_triggered_2 = false;
-  
+
 int count = 1;
 
 int[] states = {0, 1, 2, 3};
@@ -89,57 +97,200 @@ void loop() {
   if_triggered_1 = setTriggerTime(1, distance1);
   if_triggered_2 = setTriggerTime(2, distance2);
   //if(!if_triggered_1)if_triggered_1 = setTriggerTime(1, distance1);
-  if(!if_triggered_1){
-    sensorVal1 = analogRead(analogPin1);
-    distance1 = getDistance(sensorVal1);
-    if_triggered_1 = setTriggerTime(1, distance1);
-  }
+  // if(!if_triggered_1){
+  //   sensorVal1 = analogRead(analogPin1);
+  //   distance1 = getDistance(sensorVal1);
+  //   if_triggered_1 = setTriggerTime(1, distance1);
+  // }
 
-  if(current_state == 4) checkSensor();
-  
+  //if(current_state == 4) checkSensor();
+
   switch(current_state){
-    case 0:
-      if(if_triggered_1 == 0 && if_triggered_2 == 0) current_state = states[0];
-      else if(if_triggered_1 == 0 && if_triggered_2 == 1) current_state = states[1];
-      else if(if_triggered_1 == 1 && if_triggered_2 == 0) current_state = states[0];
-      else if(if_triggered_1 == 1 && if_triggered_2 == 1) current_state = states[0];
+    // 0,0
+    case STATE_INIT:
+      // if nothing is triggered, stay in state init
+      if(if_triggered_1 == 0 && if_triggered_2 == 0)
+      {
+        current_state = STATE_INIT;
+      }
+     // if 2nd sensor is triggered, begin the exit sequence
+      else if(if_triggered_1 == 0 && if_triggered_2 == 1)
+      {
+        current_state = STATE_EXIT_1;
+      }
+      // if 1st sensor is triggerec, begin the entry sequence
+      else if(if_triggered_1 == 1 && if_triggered_2 == 0)
+      {
+        current_state = STATE_ENTRY_1;
+      }
+      // this shouldnt happen, but if it does we might need to make a new state
+      else if(if_triggered_1 == 1 && if_triggered_2 == 1)
+      {
+        current_state = STATE_INIT;
+      }
       break;
-    case 1:
-      if(if_triggered_1 == 0 && if_triggered_2 == 0) current_state = states[0];
-      else if(if_triggered_1 == 0 && if_triggered_2 == 1) current_state = states[0];
-      else if(if_triggered_1 == 1 && if_triggered_2 == 0) current_state = states[0];
-      else if(if_triggered_1 == 1 && if_triggered_2 == 1) current_state = states[2];
+    // 0,1
+    case STATE_EXIT_1:
+      Serial.println("EXIT SEQUENCE BEGINNING ... REAR SENSOR TRIGGERED");
+      // goes back to init state, not a full sequence
+      if(if_triggered_1 == 0 && if_triggered_2 == 0)
+      {
+        current_state = STATE_INIT;
+      }
+      // still in the beginning of the exit phase
+      else if(if_triggered_1 == 0 && if_triggered_2 == 1)
+      {
+        current_state = STATE_EXIT_1;
+      }
+      // this would be if we skipped the double sensor phase and went straight until the end
+      // this shouldnt happen, go back to normal
+      else if(if_triggered_1 == 1 && if_triggered_2 == 0)
+      {
+        current_state = STATE_INIT;
+      }
+      // both sensors are triggered, middle of exit
+      else if(if_triggered_1 == 1 && if_triggered_2 == 1)
+      {
+        current_state = STATE_EXIT_2;
+      }
       break;
-    case 2:
-      if(if_triggered_1 == 0 && if_triggered_2 == 0) current_state = states[0];
-      else if(if_triggered_1 == 0 && if_triggered_2 == 1) current_state = states[0];
-      else if(if_triggered_1 == 1 && if_triggered_2 == 0) current_state = states[3];
-      else if(if_triggered_1 == 1 && if_triggered_2 == 1) current_state = states[0];
-      break; 
-    case 3:
-      if(if_triggered_1 == 0 && if_triggered_2 == 0) current_state = states[0];
-      else if(if_triggered_1 == 0 && if_triggered_2 == 1) current_state = states[0];
-      else if(if_triggered_1 == 1 && if_triggered_2 == 0) current_state = states[0];
-      else if(if_triggered_1 == 1 && if_triggered_2 == 1) current_state = states[0];
-      break;     
+    // 1,1
+    case STATE_EXIT_2:
+      Serial.println("EXIT SEQUENCE MIDDLE ... BOTH SENSORS TRIGGERED");
+      // maybe this should be an exit?  maybe go to state 3
+      if(if_triggered_1 == 0 && if_triggered_2 == 0)
+      {
+        current_state = STATE_INIT;
+      }
+      else if(if_triggered_1 == 0 && if_triggered_2 == 1)
+      {
+        current_state = STATE_INIT;
+      }
+      else if(if_triggered_1 == 1 && if_triggered_2 == 0)
+      {
+        current_state = STATE_EXIT_3;
+      }
+      // stay in exit 2 while they are both triggered
+      else if(if_triggered_1 == 1 && if_triggered_2 == 1)
+      {
+        current_state = STATE_EXIT_2;
+      }
+      break;
+    // 1,0
+    case STATE_EXIT_3:
+      Serial.println("EXIT SEQUENCE ENDING ... FRONT SENSOR TRIGGERED");
+      // there has been a complete exit, upload to the database
+      if(if_triggered_1 == 0 && if_triggered_2 == 0)
+      {
+        Serial.println("EXIT SEQUENCE COMPLETE .... UPLOADING TO DATABASE");
+        // upload exit to database
+        current_state = STATE_INIT;
+        break;
+      }
+
+      else if(if_triggered_1 == 0 && if_triggered_2 == 1)
+      {
+        current_state = STATE_INIT;
+      }
+      // stay on exit 3 if the exit has not completed yet
+      else if(if_triggered_1 == 1 && if_triggered_2 == 0)
+      {
+        current_state = STATE_EXIT_3;
+      }
+      // this shouldnt happen hopefully
+      else if(if_triggered_1 == 1 && if_triggered_2 == 1)
+      {
+        current_state = STATE_INIT;
+      }
+      Serial.println("ENTRY SEQUENCE DID NOT COMPLETE");
+      break;
+    // 1,0
+    case STATE_ENTRY_1:
+      Serial.println("ENTRY SEQUENCE BEGINNING ... FRONT SENSOR TRIGGERED");
+      if(if_triggered_1 == 0 && if_triggered_2 == 0)
+      {
+        current_state = STATE_INIT;
+      }
+      else if(if_triggered_1 == 0 && if_triggered_2 == 1)
+      {
+        current_state = STATE_INIT;
+      }
+      else if(if_triggered_1 == 1 && if_triggered_2 == 0)
+      {
+        current_state = STATE_ENTRY_1;
+      }
+      else if(if_triggered_1 == 1 && if_triggered_2 == 1)
+      {
+        current_state = STATE_ENTRY_2;
+      }
+      break;
+    // 1,1
+    case STATE_ENTRY_2:
+    Serial.println("ENTRY SEQUENCE MIDDLE ... BOTH SENSORS TRIGGERED");
+      // same as for STATE_EXIT_2 maybe this should be considered an entry
+      if(if_triggered_1 == 0 && if_triggered_2 == 0)
+      {
+        // might need to add database entry upload code here
+        // current_state = STATE_ENTRY_3
+        current_state = STATE_INIT;
+      }
+      else if(if_triggered_1 == 0 && if_triggered_2 == 1)
+      {
+        current_state = STATE_INIT;
+      }
+      else if(if_triggered_1 == 1 && if_triggered_2 == 0)
+      {
+        current_state = STATE_ENTRY_3;
+      }
+      else if(if_triggered_1 == 1 && if_triggered_2 == 1)
+      {
+        current_state = STATE_ENTRY_2;
+      }
+      break;
+    // 0,1
+    case STATE_ENTRY_3:
+      Serial.println("ENTRY SEQUENCE END ... REAR SENSOR TRIGGERED");
+      if(if_triggered_1 == 0 && if_triggered_2 == 0)
+      {
+        Serial.println("EXIT SEQUENCE COMPLETE .... UPLOADING TO DATABASE");
+        // send an entry to the database
+        current_state = STATE_INIT;
+        break;
+      }
+      else if(if_triggered_1 == 0 && if_triggered_2 == 1)
+      {
+        current_state = STATE_ENTRY_3;
+      }
+      else if(if_triggered_1 == 1 && if_triggered_2 == 0)
+      {
+        // this really shouldnt happen
+        current_state = STATE_INIT;
+      }
+      // maybe this should go back to STATE_ENTRY_2
+      else if(if_triggered_1 == 1 && if_triggered_2 == 1)
+      {
+        current_state = STATE_ENTRY_2;
+      }
+      Serial.println("ENTRY SEQUENCE DID NOT COMPLETE");
+      break;
   }
 
-  
 
-  
+
+
 //   if(if_triggered_1 && if_triggered_2) checkSensor();
 //   else if(if_triggered_1 && !if_triggered_2){
 //     time1 = 0;
-//     time2 = 0; 
+//     time2 = 0;
 //   }
 //   else if(!if_triggered_1 && if_triggered_2){
 //     time1 = 0;
-//     time2 = 0; 
+//     time2 = 0;
 //   }
    if_triggered_1 = false;
    if_triggered_2 = false;
   delay(200);
- Serial.println(); 
+ Serial.println();
 }
 
 int getDistance(int sensorVal){
@@ -149,7 +300,7 @@ int getDistance(int sensorVal){
   Serial.print(distance);
   Serial.println(" cm");
   Serial.println();
-  return distance;  
+  return distance;
 }
 
 bool setTriggerTime(int id, float distance){
@@ -164,7 +315,7 @@ bool setTriggerTime(int id, float distance){
     }
     else{
       time2 = millis();
-      Serial.println("reached 2 first"); 
+      Serial.println("reached 2 first");
 //      Serial.print("Second Sensor Time 1: ");
 //      Serial.println(time1);
 //      Serial.print("Time 2: ");
